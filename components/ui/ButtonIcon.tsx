73c9fed411b5f71b5232f3904cb210bdc145121a
Soumendu Nayak <guitar.soumendu@gmail.com>
2025-11-19 13:57:05 +0530

    ButtonIcon animation added

--- components/ui/ButtonIcon.tsx
+++ components/ui/ButtonIcon.tsx
@@ -3,6 +3,11 @@ import { Icons } from "@/contants/Icons";
 import { Image } from "expo-image";
 import React from "react";
 import { Pressable, PressableProps, StyleSheet } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 
 interface ButtonIconProps extends PressableProps {
   icon: keyof typeof Icons;
@@ -10,29 +15,47 @@ interface ButtonIconProps extends PressableProps {
 }
 
 const ButtonIcon = (props: ButtonIconProps) => {
+  const pressableOpacity = useSharedValue(1);
+  const pressableAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: pressableOpacity.value,
+    };
+  }, []);
+
+  const pressableFadeIn = () => {
+    pressableOpacity.value = withSpring(1);
+  };
+  const pressableFadeOut = () => {
+    pressableOpacity.value = withSpring(0.5);
+  };
+
   return (
-    <Pressable
-      {...props}
-      style={(state) => [
-        {
-          backgroundColor:
-            props.variant === "primary" ? Colors.primary : Colors.secondary,
-        },
-        styles.container,
-        typeof props.style === "function" ? props.style(state) : props.style,
-      ]}
-    >
-      <Image
-        source={props.icon}
-        style={[
+    <Animated.View style={pressableAnimatedStyle}>
+      <Pressable
+        {...props}
+        onPressIn={pressableFadeOut}
+        onPressOut={pressableFadeIn}
+        style={(state) => [
           {
-            tintColor:
-              props.variant === "primary" ? Colors.white : Colors.black,
+            backgroundColor:
+              props.variant === "primary" ? Colors.primary : Colors.secondary,
           },
-          styles.icon,
+          styles.container,
+          typeof props.style === "function" ? props.style(state) : props.style,
         ]}
-      />
-    </Pressable>
+      >
+        <Image
+          source={props.icon}
+          style={[
+            {
+              tintColor:
+                props.variant === "primary" ? Colors.white : Colors.black,
+            },
+            styles.icon,
+          ]}
+        />
+      </Pressable>
+    </Animated.View>
   );
 };
 
