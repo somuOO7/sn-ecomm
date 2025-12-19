53dd87c82dd8fa5e8448d529b31a9cb375522e01
Soumendu Nayak <guitar.soumendu@gmail.com>
2025-12-19 16:33:44 +0530

    Added bottom sheet for cart success screen

--- app/(common)/success.tsx
+++ app/(common)/success.tsx
@@ -1,20 +1,51 @@
 import Button from "@/components/ui/Button";
 import Label from "@/components/ui/Label";
 import SafeContianer from "@/components/ui/SafeContianer";
+import { Colors } from "@/contants/Colors";
+import { Sizes } from "@/contants/Sizes";
 import { router } from "expo-router";
 import LottieView from "lottie-react-native";
-import React from "react";
-import { StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 
 const success = () => {
+  const [isBottomSheetVisible, setIsBottomSheetVisible] = useState(false);
+  const bottomSheetFlex = useSharedValue(0);
+  const bottomSheetAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      flex: bottomSheetFlex.value,
+    };
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsBottomSheetVisible(true);
+      bottomSheetFlex.value = withSpring(1);
+    }, 3000);
+  }, []);
+
   return (
-    <SafeContianer style={styles.container}>
+    <SafeContianer variant="secondary" style={styles.container}>
+      <Label variant="bold" style={{ fontSize: 24 }}>
+        Order Successful!
+      </Label>
       <LottieView
+        autoPlay
         loop={false}
         style={styles.successIcon}
         source={require("@/assets/animation/success.json")}
       />
-      <Label variant="bold">Order Placed Successfully</Label>
+      <Label style={{ textAlign: "center" }}>
+        Your order is being packed now by the App, and will be ready for
+        shipping soon.
+      </Label>
+
+      <View style={{ height: 24 }} />
 
       <Button
         title="Back to home"
@@ -24,6 +55,11 @@ const success = () => {
         }}
         variant="primary"
       />
+      {isBottomSheetVisible && (
+        <Animated.View style={[styles.bottomSheet, bottomSheetAnimatedStyle]}>
+          <Label>Continue shopping</Label>
+        </Animated.View>
+      )}
     </SafeContianer>
   );
 };
@@ -31,10 +67,18 @@ const success = () => {
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
+    justifyContent: "center",
   },
   successIcon: {
     width: "100%",
     height: 200,
+    marginVertical: -38,
+  },
+  bottomSheet: {
+    backgroundColor: Colors.white,
+    width: "100%",
+    borderTopLeftRadius: Sizes.borderRadius,
+    borderTopRightRadius: Sizes.borderRadius,
   },
 });
 
