5e403b2e4951daa9b04a4db128ebc306da7772a4
Soumendu Nayak <guitar.soumendu@gmail.com>
2025-12-22 00:00:28 +0530

    ScrollView added in dashboard

--- app/(dashboard)/index.tsx
+++ app/(dashboard)/index.tsx
@@ -11,7 +11,7 @@ import { useLoader } from "@/stores/loaderState";
 import { useProduct } from "@/stores/productStore";
 import { useFocusEffect } from "expo-router";
 import { useCallback } from "react";
-import { View } from "react-native";
+import { ScrollView, View } from "react-native";
 
 export default function Index() {
   const { setIsLoading } = useLoader();
@@ -37,49 +37,56 @@ export default function Index() {
   return (
     <SafeContianer>
       <DashboardTopHeader />
-      <SearchWithFilter />
 
-      <SectionTitle title="Categories" />
-      <CategorySelector />
+      <ScrollView
+        style={{ flex: 1 }}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ gap: 16 }}
+      >
+        <SearchWithFilter />
 
-      <MultiBannerLarge
-        bannerList={[
-          {
-            id: "123",
-            title: "Get Your Special Sale Upto 40%",
-            imageUrl: require("@/assets/images/dummy/dummy-1.png"),
-            actionTitle: "Shop Now",
-          },
-          {
-            id: "1234",
-            title: "Test2",
-            imageUrl: require("@/assets/images/dummy/dummy-2.png"),
-          },
-          {
-            id: "12345",
-            title: "Test3",
-            imageUrl: require("@/assets/images/dummy/dummy-1.png"),
-            actionTitle: "Shop Now",
-          },
-          {
-            id: "123456",
-            title: "Test4",
-            imageUrl: require("@/assets/images/dummy/dummy-2.png"),
-          },
-        ]}
-      />
+        <SectionTitle title="Categories" />
+        <CategorySelector />
 
-      <SectionTitle title="Popular Products" />
-      <View style={{ flexDirection: "row", gap: 16, flexWrap: "wrap" }}>
-        {products.map((product) => (
-          <GridCard
-            key={product.id}
-            title={product.title}
-            price={product.price}
-            imageUrl={product.imageUrl}
-          />
-        ))}
-      </View>
+        <MultiBannerLarge
+          bannerList={[
+            {
+              id: "123",
+              title: "Get Your Special Sale Upto 40%",
+              imageUrl: require("@/assets/images/dummy/dummy-1.png"),
+              actionTitle: "Shop Now",
+            },
+            {
+              id: "1234",
+              title: "Test2",
+              imageUrl: require("@/assets/images/dummy/dummy-2.png"),
+            },
+            {
+              id: "12345",
+              title: "Test3",
+              imageUrl: require("@/assets/images/dummy/dummy-1.png"),
+              actionTitle: "Shop Now",
+            },
+            {
+              id: "123456",
+              title: "Test4",
+              imageUrl: require("@/assets/images/dummy/dummy-2.png"),
+            },
+          ]}
+        />
+
+        <SectionTitle title="Popular Products" />
+        <View style={{ flexDirection: "row", gap: 16, flexWrap: "wrap" }}>
+          {products.map((product) => (
+            <GridCard
+              key={product.id}
+              title={product.title}
+              price={product.price}
+              imageUrl={product.imageUrl}
+            />
+          ))}
+        </View>
+      </ScrollView>
     </SafeContianer>
   );
 }
